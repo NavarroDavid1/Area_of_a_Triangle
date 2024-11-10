86e815ae639daec3eee2412b313b526a127b53a4
David Navarro <dnavarro51@student.rccd.edu>
2024-11-10 10:21:49 -0800

    Update show_results.cpp

--- show_results.cpp
+++ show_results.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <iomanip>
 
-extern "C" void show_results(double side1, double side2, double side3, double area) {
-    std::cout << std::fixed << std::setprecision(10);
-    std::cout << "The area of a triangle with sides "
-              << side1 << ", "
-              << side2 << ", and "
-              << side3 << " is "
-              << area << " square units.\n";
+extern "C" {
+    void show_results(double side1, double side2, double side3, double area);
 }
 
+void show_results(double side1, double side2, double side3, double area) {
+    std::cout << "The area of a triangle with sides "
+              << std::fixed << std::setprecision(10)
+              << side1 << ", " << side2 << ", and " << side3
+              << " is " << area << " square units.\n";
+}
